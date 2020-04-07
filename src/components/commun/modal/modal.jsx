5916bdcca33c523ea6d3215376091a775eb97882
kokou kpadenou <kkokou@gmail.com>
2020-04-07 08:49:40 -0400

    modal component

---
+++ src/components/commun/modal/modal.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Modal = ({ title, children, modalId }) => {
+  return (
+    <div
+      className="modal fade"
+      id={modalId}
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby={`${modalId}Label`}
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-lg" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id={`${modalId}Label`}>
+              {title}
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">{children}</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;

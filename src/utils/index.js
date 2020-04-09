6b72e014493081dfff84b78e7a2924ccfb4d7b5c
kokou kpadenou <kkokou@gmail.com>
2020-04-09 00:18:28 -0400

    dateFormating function added to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,9 @@
 import _ from "lodash";
 
+export const dateFormating = (date) => {
+  return date ? new Date(date).toDateString().slice(4) : "";
+};
+
 export function paginate(items, pageNumber, pageSize) {
   const startIndex = (pageNumber - 1) * pageSize;
   return _(items).slice(startIndex).take(pageSize).value();

4fbba6df226b5f715f408f233d7a6fb544e90c7c
kokou kpadenou <kkokou@gmail.com>
2020-04-09 00:21:13 -0400

    tableBody updated to fix key error

--- src/components/commun/table/tableBody.jsx
+++ src/components/commun/table/tableBody.jsx
@@ -4,8 +4,8 @@ import TableLine from "./tableLine";
 const TableBody = ({ data, columns }) => {
   return (
     <tbody>
-      {data.map(item => (
-        <TableLine key={item.label} data={item} columns={columns} />
+      {data.map((item) => (
+        <TableLine key={item._id} data={item} columns={columns} />
       ))}
     </tbody>
   );
